853006fb16683f6bba37c4c771187033d9b339ee
cai980273879 <980273879@qq.com>
2017-09-29 12:15:46 +0800

    add "SimpleDataBaseSystem.java"

--- day09/src/com/icss/teacher/SimpleDataBaseSystem.java
+++ day09/src/com/icss/teacher/SimpleDataBaseSystem.java
@@ -1,60 +1,61 @@
-
 package com.icss.teacher;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Properties;
 import java.util.Scanner;
 
-
-/**
- 编写一个学生类（学号、姓名、出生日期、性别、身高、考试成绩）
-
- 编写一个教师类（工号、姓名、出生日期、性别、工龄、绩效等级）
-
- 创建多个（>2）学生对象，并保存成配置文件
- 创建多个(>1)教师对象，并保存成配置文件
-
- 编写一个方法，使用反射，调用对象的setter方法，设置对象属性
-
- 编写一个方法，将各配置文件读取成学生对象和教师对象
- */
-
 public class SimpleDataBaseSystem {
-    private static File root = new File("~/");
+
+    private static File root = new File("~/Documents");
+
     private static Scanner sc;
 
     public static void main(String[] args) {
+
         printMenu();
         sc = new Scanner(System.in);
         String cmd = null;
-        while (true){
+        while(true) {
             cmd = sc.nextLine();
-            if(cmd.equalsIgnoreCase("exit")){
+            if(cmd.equalsIgnoreCase("exit")) {
                 return;
             }
-            if(cmd.equalsIgnoreCase("meau")){
+
+            if(cmd.equalsIgnoreCase("menu")) {
                 printMenu();
                 continue;
             }
-            if(cmd.startsWith("add ")){
-                try{
 
-                }catch (Exception e){
-                    System.out.println("添加失败"+e.getMessage());
+            if(cmd.startsWith("add ")) {
+                try{
+                    add(cmd.substring(3).trim());
+                }catch(Exception ex){
+                    System.out.println("添加失败："+ex.getMessage());
                 }
                 continue;
             }
 
             String regex = "^read +([^ ]+) +([^ ]+)$";
-            if(cmd.matches(regex)){
-                try {
-                    Object obj = readObjectFromProperties();
+            if(cmd.matches(regex)) {
+                try{
+                    Object obj = readObjectFromProperties(cmd.replaceAll(regex, "$1"), cmd.replaceAll(regex, "$2"));
+                    printObject(obj);
+                }catch(Exception ex){
+                    System.out.println("读取失败："+ex.getMessage());
                 }
+                continue;
             }
+
+            System.out.println("未知的命令");
+            System.out.println();
+
         }
     }
+
     private static void printMenu() {
         System.out.println("-------------------------");
         System.out.println("\tmenu:打印菜单");
@@ -64,51 +65,59 @@ public class SimpleDataBaseSystem {
         System.out.println("-------------------------");
     }
 
-    private static void add(String className)throws Exception{
-
-        //获取需要添加的类
 
 
+    private static void add(String className)throws Exception {
+
+        /**
+         * 获取要添加的类
+         */
         Class<?> cls = null;
         try{
-            cls = Class.forName("com.icss.teacher."+className);
-        }catch (Exception e){
+            cls = Class.forName("com.icss."+className);
+        }catch(Exception ex){
             System.out.println("类com.icss."+className+"不存在");
         }
 
+        /**
+         * 实例化要添加的对象
+         */
+        Object obj = cls.newInstance();
 
-        /*实例化要添加的对象*/
-        Object obj = cls.newInstance();//无参构造
-        int x = Integer.valueOf(arg0);
+//		int a = Integer.valueOf(arg0)
 
-
-        /*遍历所有公开的方法*/
-        for (Method m : cls.getMethods()){
+        //遍历所有公开的方法
+        for(Method m : cls.getMethods()){
+            //获取setter方法
             if(m.getName().startsWith("set")&&m.getParameterTypes().length==1){
                 String name = m.getName();//获取方法名
-                name = name.substring(3);//获取"setXXXX"中的"XXXX"
-                name = name.substring(0,1).toLowerCase()+name.substring(1);
+                name = name.substring(3);//获取对应的字段名
+                name = name.substring(0, 1).toLowerCase()+name.substring(1);
                 System.out.println("请输入"+name+":");
-                String value = sc.nextLine();
-                m.invoke(obj,value);//执行setter
+                String value = sc.nextLine().trim();
+                m.invoke(obj, value);//执行setter
             }
+
         }
+
         try{
             saveObjectToProperties(obj);
             System.out.println("保存数据成功");
             printObject(obj);
-        }catch (Exception e){
-            saveObjectToProperties("保存数据失败"+e.getMessage());
+        }catch(Exception ex){
+            System.out.println("保存数据失败："+ex.getMessage());
         }
 
-    }
 
+
+
+    }
 
     /**
      * 保存一个对象到配置文件中
      * @param obj
      */
-    private static void saveObjectToProperties(Object obj) throws Exception{
+    private static void saveObjectToProperties(Object obj)throws Exception{
         File path = new File(root,obj.getClass().getSimpleName());
         path.mkdir();
 
@@ -117,31 +126,90 @@ public class SimpleDataBaseSystem {
         Method getId = cls.getMethod("getId");//获取主键方法
         String id = (String)getId.invoke(obj);//获取主键
 
-        if(id == null){
+        if(id==null){
             throw new Exception("主键不能为空");
         }
+
         if(new File(path,id).exists()){
-            throw new Exception("数据已经存在");
+            throw new Exception("该数据已经存在");
         }
 
-        Properties config = new Properties();
-        //生成properties对象
+        Properties one = new Properties();
 
         //遍历所有公开的方法
         for(Method m : obj.getClass().getMethods()){
             //获取getter方法
             if(m.getName().startsWith("get")&&m.getParameterTypes().length==0){
-                String name = m.getName();//获取方法名
-                if(name.equals("getClass")){
-                    continue;
-                }//不需要输出class属性
-                name = name.substring(3);//比如说是getXXX那么获取到的就是XXX
-                name = name.substring(0,1).toLowerCase()+name.substring(1);
 
-                String value = (String)m.invoke(obj);//执行getter方法
-                config.setProperty(name,value);//
+                String name = m.getName();//获取方法名
+                if(name.equals("getClass")){//不需要输出class属性
+                    continue;
+                }
+                name = name.substring(3);//获取对应的字段名
+                name = name.substring(0, 1).toLowerCase()+name.substring(1);
+
+                String value = (String)m.invoke(obj);//执行getter
+                one.setProperty(name, value);
             }
         }
-        config.store(new FileWriter(new File(path,id)),null);
+        one.store(new FileWriter(new File(path,id)), null);
+    }
+
+    private static Object readObjectFromProperties(String className,String id)throws Exception{
+        /**
+         * 获取要添加的类
+         */
+        Class<?> cls = null;
+        try{
+            cls = Class.forName("com.icss."+className);
+        }catch(Exception ex){
+            System.out.println("类com.icss."+className+"不存在");
+        }
+
+        /**
+         * 实例化要添加的对象
+         */
+        Object obj = cls.newInstance();
+
+        Properties one = new Properties();
+        one.load(new FileReader(new File(root,className+"/"+id)));
+
+        //遍历所有公开的方法
+        for(Method m : cls.getMethods()){
+            //获取setter方法
+            if(m.getName().startsWith("set")&&m.getParameterTypes().length==1){
+                String name = m.getName();//获取方法名
+                name = name.substring(3);//获取对应的字段名
+                name = name.substring(0, 1).toLowerCase()+name.substring(1);
+
+                String value = one.getProperty(name);
+                m.invoke(obj, value);//执行setter
+            }
+
+        }
+        return obj;
+    }
+
+    private static void printObject(Object obj)throws Exception{
+        System.out.println("--------------------------------");
+        System.out.println("\t数据名称："+obj.getClass().getSimpleName());
+        //遍历所有公开的方法
+        for(Method m : obj.getClass().getMethods()){
+            //获取getter方法
+            if(m.getName().startsWith("get")&&m.getParameterTypes().length==0){
+
+                String name = m.getName();//获取方法名
+                if(name.equals("getClass")){//不需要输出class属性
+                    continue;
+                }
+                name = name.substring(3);//获取对应的字段名
+                name = name.substring(0, 1).toLowerCase()+name.substring(1);
+
+                Object value = m.invoke(obj);//执行getter
+                System.out.println("\t"+name+":"+value);
+            }
+        }
+        System.out.println("--------------------------------");
+
     }
 }
